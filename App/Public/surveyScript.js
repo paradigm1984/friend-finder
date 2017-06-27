ced3aa3d465fa459946512a1642cdcb76cfd4874
paradigm1984 <danblanco1990@gmail.com>
2017-06-27 13:37:40 -0400

    worked on apiRoutes.js logic

---
+++ App/Public/surveyScript.js
@@ -0,0 +1,27 @@
+
+// on submit, make the modal appear with the name and photo of your best match.
+
+$("#submit").on("click", function() {
+
+	// puts the user input into an object
+	var userInput = {
+
+		name: $("name").val().trim(),
+		photo: $("photo").val().trim(),
+		scores: [$('#question01').val().trim(), $('#question02').val().trim(), $('#question03').val().trim(), $('#question04').val().trim(), $('#question05').val().trim(), $('#question06').val().trim(), $('#question07').val().trim(), $('#question08').val().trim(), $('#question09').val().trim(), $('#question10').val().trim()]
+	}
+
+	// puts the current url into a variable
+	var currentURL = window.location.origin;
+
+	// uses jquery to make a post request
+	$.post(currentURL + "/api/friends", userInput, function(data) {
+
+		$("#userName").text(data.name);
+		$("#userImage").attr("src", data.photo);
+		$("#bestFriendModal").modal("toggle");
+	});
+
+	return false;
+
+});
--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -1,4 +1,62 @@
 // LOOK INTO: https://github.com/lvanok/FriendFinder
+// ALSO: https://github.com/lvanok/FriendFinder/blob/master/app/routing/api-routes.js
 
 var friendData = require("../friends.js");
 var path = require("path");
+
+module.exports = function(app) {
+
+	app.get("/api/friends", function(req, res){
+		res.json(friends);
+	});
+
+	app.post('/api/friends', function(req, res){
+
+		// makes an object for the perfect match
+		var perfectMatch = {
+
+			name: "",
+			image: "",
+			matchDifference: 500
+		};
+
+		// grabs the data inputted by the user and
+		// assigns them variables
+		var newFriendData = req.body;
+		var newFriendName = userInput.name;
+		var newFriendPhoto = userInput.photo;
+		var newFriendScores = userInput.scores;
+
+		var totalDifference = 0;
+
+		for (var i = 0; i < [friends].length-1; i++) {
+			
+			console.log(friends[i].name);
+
+			for (var a = 0; a < 10; a++) {
+
+				// getting the total difference by subtracting the difference between each answer choice in teh array
+				totalDifference += Math.abs(parseInt(newFriendScores[a]) - parseInt(friends[i].scores[a]));
+
+				// if that difference is less than or equal to the matchDifference which it is because i set it high
+				if(totalDifference <= perfectMatch.matchDifference) {
+
+					greatMatch.name = friends[i].name;
+					greatMatch.photo = friends[i].photo;
+					greatMatch.matchDifference = totalDifference;
+				}
+
+			}
+		}	
+
+		friends.push(newFriendData);
+		res.json(greatMatch);
+	});
+
+
+
+
+
+
+
+
