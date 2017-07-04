4643698710f74e46aea550d0d7c90d0f8a4abb1a
paradigm1984 <danblanco1990@gmail.com>
2017-07-03 23:05:09 -0400

    got everything to work besides actually posting the modal with the good match

--- App/Data/friends.js
+++ App/Data/friends.js
@@ -1,19 +1,19 @@
 var friends = [
 
 {
-"name": "Dan",
-"photo": "http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/mallard-male-standing.adapt.945.1.jpg,",
-"scores":[
- 4,
- 2,
- 4,
- 1,
- 5,
- 1,
- 2,
- 3,
- 5,
- 2
+'name': 'Dan',
+'photo': 'http://kids.nationalgeographic.com/content/dam/kids/photos/animals/Birds/H-P/mallard-male-standing.adapt.945.1.jpg,',
+'scores':[
+ '4',
+ '2',
+ '4',
+ '1',
+ '5',
+ '1',
+ '2',
+ '3',
+ '5',
+ '2'
   ]
 
 }
--- App/Routing/apiRoutes.js
+++ App/Routing/apiRoutes.js
@@ -1,6 +1,6 @@
 
-var friendData = require("../Data/friends.js");
 var path = require("path");
+var friendData = require("../Data/friends.js");
 
 
 module.exports = function(app) {
@@ -8,22 +8,48 @@ module.exports = function(app) {
 	// this populates the localhost:3000/api/friends
 	app.get("/api/friends", function(req, res){
 		res.json(friendData);
-	});
 
+	});
+	
 	app.post("/api/friends", function(req, res){
 
 		// an object for a good match starting empty
 		var goodMatch = {
 			name: "",
-			image: "",
+			photo: "",
 			matchDifference: 500
 		};
 
-		console.log(req.body);
-		var userData = req.body;
+		var userData 	= req.body;
+		// splits up the req.body
+		var userName 	= userData.name;
+		var userImage 	= userData.photo;
+		var userScores 	= userData.scores;
+		// console.log("user name: " + userName + ", " + "user image: " + userImage + ", " + "user scores: " + userScores);
+		// console.log("------------------------------------------------------");
+		var totalDifference = 0;
 
+		for (var i = 0; i < friendData.length; i++) {
+			// console.log(friendData[i].name);
+			// console.log("------------------------------------------------------");
 
+			for (var j = 0; j < userScores.length; j++) {
+				totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(friendData[i].scores[j]));
+			}
+			console.log("total difference: " + totalDifference);
+			if (totalDifference <= goodMatch.matchDifference){
 
-	});
+					// Reset the bestMatch to be the new friend. 
+					goodMatch.name = friendData[i].name;
+					a = friendData[i].photo;
+					goodMatch.matchDifference = totalDifference;
+					console.log(goodMatch);
+			} // end if statement
+		} // end for loop
 
-};
+		friendData.push(userData);
+		res.json(goodMatch);
+
+	}); // end app.post
+
+}; // end module.exports
